bd4a340231823972a0ff76ee5f676833f95ccd7f
potato-na <m.0305.ngm@gmail.com>
2021-09-17 18:16:37 +0900

    #3 separate class to another file

--- create/Singleton/src/main_global.py
+++ create/Singleton/src/main_global.py
@@ -1,10 +1,4 @@
-class Config:
-	def __init__(self):
-		self.model_name = "hoge"
-
-	def set_name(self, name):
-		self.model_name = name
-
+from model.config import Config
 
 CONFIG = Config()
 
--- create/Singleton/src/main_prohibit_new.py
+++ create/Singleton/src/main_prohibit_new.py
@@ -1,30 +1,4 @@
-from threading import Lock
-
-class Config:
-	_unique_instance = None
-	_lock = Lock() # クラスロック
-
-	model_name = "hoge"
-
-	def __new__(cls):
-		raise NotImplementedError("Cannot initialize via Constructor")
-
-	@classmethod
-	def __internal_new__(cls):
-		return super().__new__(cls)
-
-	@classmethod
-	def get_instance(cls):
-		if not cls._unique_instance:
-			with cls._lock:
-				if not cls._unique_instance:
-					cls._unique_instance = cls.__internal_new__()
-		return cls._unique_instance
-	
-	@classmethod
-	def set_name(cls, name):
-		cls.model_name = name
-
+from model.config_prohibit_new import Config
 
 def main():
 	class Caller0:
